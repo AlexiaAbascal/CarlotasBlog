50eb5cd6d73af47fe53c77c9f2bee561d83187fc
AlexiaAbascal <alexiaabascal@hotmail.com>
2025-01-29 11:28:44 -0700

    template card

--- src/components/PatternCard.vue
+++ src/components/PatternCard.vue
@@ -1,47 +1,39 @@
-
 <template>
-    
     <div class="container">
-        <div class="row">
-            <span style="background-color: black;"> hola</span>
-            
-            <div class="col-md-2" style="background-color: red;">
-                <p style="background-color: black;"> hola</p>          
-            </div>
-        </div>
-    </div>
-<!-- 
-    <div class="d-flex justify-content-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2"></div>
-
-            <div class="col-md-8">
-                <div class="card mb-3" > 
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="/src/assets/cable_beanie.jpg" class="beanie img-fluid rounded-start" alt="cable beanie">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h3 class="card-title">Cable Beanie</h3>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-body-secondary">Level : Intermediate</small></p>
-                            </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-8 card">
+                <div class="row g-0">
+                    <div class="col-md-2 d-flex">
+                        <img src="/src/assets/cable_beanie.jpg" class="img-fluid rounded-start" alt="cable beanie">
+                    </div>
+                    <div class="col-md-10">
+                        <div class="card-body">
+                            <h3 class="card-title">Cable Beanie</h3>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-body-secondary">Level : Intermediate</small></p>
                         </div>
                     </div>
                 </div>
             </div>
-
-            
         </div>
     </div>
-</div> -->
-
 </template>
 
 <style scoped>
-    .bg-black{
-        background-color:black;
-    }
+
+.card-body {
+    padding: 0px;
+    margin-left: 6px; 
+}
+
+.card{
+    padding: 0px;
+    margin: 0px;
+}
+
+img{
+    object-fit: cover;
+}
+
 </style>
